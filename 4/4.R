9a9fe3b18b4607d9c9f86b70997d370863bacf38
Amir Kurbanov <ku.amir73@gmail.com>
2024-03-13 16:01:40 +0300

    changed 4

--- 4/4.R
+++ 4/4.R
@@ -1,27 +1,15 @@
 #====================================Часть 1====================================
 #___________________________________Задание 1___________________________________
-# install.packages("xslx")
-# install.packages("lubridate")
-library(xlsx)
+# install.packages('readxl')
+# install.packages('lubridate')
+library(readxl)
 library(lubridate)
-column_classes <- c("Date", "numeric", "numeric", "numeric", "numeric",
-                    "numeric", "numeric", "character", "character")
-column_names <- c("Date", "Preasure_MPa", "Temp_°C", "Gas_Prod_m3_per_d", 
-                  "Cond_m3_per_d", "Water_m3_per_d", "ID", "Bush", "Group")
-gas <- read.xlsx("GAZ.xlsx", 
-                 sheetIndex = 1, 
-                 colNames = FALSE, 
-                 colClasses = column_classes, 
-                 skipEmptyCols = TRUE)
-colnames(gas) <- column_names
-gas <- gas[, 1:9]
-filtered_rows <- day(gas$Date) < 13
-gas$Date <- format(as.Date(gas$Date, format = "%Y-%m-%d"), format = "%d-%m-%Y")
-gas$Date[filtered_rows] <- format(as.Date(gas$Date[filtered_rows],
-                                          format = "%d-%m-%Y"),
-                                  format = "%m-%d-%Y")
-gas$Date <- as.Date(gas$Date, format = "%d-%m-%Y")
-head(gas)
+gas <- read_excel('GAZ.xlsx', 
+                  col_types = c('numeric', 'numeric', 'numeric', 'numeric', 
+                                'numeric', 'numeric', 'text', 'text', 'text'))
+colnames(gas) <- c("Date", "Preasure_MPa", "Temp_C", "Gas_Prod_m3_per_d", 
+                   "Cond_m3_per_d", "Water_m3_per_d", "ID", "Bush", "Group")
+gas$Date <- ymd("1899-12-31") + days(gas$Date - 1)
 
 #___________________________________Задание 2___________________________________
 nrow(gas)
@@ -29,7 +17,7 @@ gas <- gas[complete.cases(gas), ]
 nrow(gas)
 
 #___________________________________Задание 3___________________________________
-colnames(gas)[colnames(gas) == "Temp_°C"] <- "Temp_K"
+colnames(gas)[colnames(gas) == "Temp_C"] <- "Temp_K"
 gas$Temp_K <- gas$Temp_K + 273.15
 head(gas)
 
@@ -47,27 +35,29 @@ head(gas)
 
 #___________________________________Задание 6___________________________________
 gas_2018 <- subset(gas, year(Date) == 2018)
-agg_data <- aggregate(Gas_Prod_m3_per_d ~ Bush, data = gas_2018, sum)
-colnames(agg_data)[2] <- "Total_Prod_2018"
-agg_data
+nrow(gas_2018)
 
 #___________________________________Задание 7___________________________________
 gas_111 = gas[gas$ID == 111, ]
-head(gas_111)
+nrow(gas_111)
 
 #___________________________________Задание 8___________________________________
 k_more_than_2 <- table(subset(gas, Water_m3_per_d > 2)$ID)
 ID_less_than_2 <- names(k_more_than_2[k_more_than_2 == 0])
 ID_less_than_2
+length(ID_less_than_2)
 
 #___________________________________Задание 9___________________________________
 k_less_than_1000 <- table(subset(gas, Gas_Prod_m3_per_d < 1000)$ID)
 ID_more_than_1000 <- names(k_less_than_1000[k_less_than_1000 == 0])
 ID_more_than_1000
+length(ID_more_than_1000)
 
-k_less_than_1000 <- table(subset(gas, Gas_Prod_m3_per_d < 13750)$Bush)
+max <- 12500
+k_less_than_1000 <- table(subset(gas, Gas_Prod_m3_per_d < max)$Bush)
 Bush_more_than_1000 <- names(k_less_than_1000[k_less_than_1000 == 0])
 Bush_more_than_1000
+max
 
 #___________________________________Задание 10__________________________________
 gas_2018 <- subset(gas, year(Date) == 2018)
@@ -94,3 +84,4 @@ max_mean_bush <- agg_data[which.max(agg_data$Mean_Gas_Water_Ratio), 1]
 max_mean_bush
 
 #_______________________________________________________________________________
+
